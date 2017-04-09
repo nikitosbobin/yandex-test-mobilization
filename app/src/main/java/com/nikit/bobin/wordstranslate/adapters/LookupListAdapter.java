f9a69c16f60e5cdb2f40bfea6bc0498774f27c15
bobin <bobin@skbkontur.ru>
2017-04-09 20:31:14 +0500

    Use ViewHolder pattern in LookupListAdapter

--- app/src/main/java/com/nikit/bobin/wordstranslate/adapters/LookupListAdapter.java
+++ app/src/main/java/com/nikit/bobin/wordstranslate/adapters/LookupListAdapter.java
@@ -12,6 +12,9 @@ import com.nikit.bobin.wordstranslate.core.Strings;
 import com.nikit.bobin.wordstranslate.translating.models.SynonymGroup;
 import com.nikit.bobin.wordstranslate.translating.models.WordLookup;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class LookupListAdapter extends BaseAdapter {
     private WordLookup lookup;
     private LayoutInflater inflater;
@@ -37,27 +40,39 @@ public class LookupListAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View view = convertView;
-        if (view == null) {
+    public View getView(int position, View view, ViewGroup parent) {
+        LookupViewHolder holder;
+        if (view != null) {
+            holder = (LookupViewHolder) view.getTag();
+        } else {
             view = inflater.inflate(R.layout.lookup_synonym_layout, parent, false);
+            holder = new LookupViewHolder(view);
+            view.setTag(holder);
         }
         SynonymGroup synonymGroup = getItem(position);
 
-        TextView synonymView = (TextView) view.findViewById(R.id.lookup_text);
-        TextView meanView = (TextView) view.findViewById(R.id.lookup_mean);
-
-        synonymView.setText(Strings.join(synonymGroup.getSynonyms(), ", "));
+        holder.synonymView.setText(Strings.join(synonymGroup.getSynonyms(), ", "));
         if (synonymGroup.getMean().length == 0) {
-            meanView.setVisibility(View.GONE);
+            holder.meanView.setVisibility(View.GONE);
         } else {
-            meanView.setVisibility(View.VISIBLE);
-            meanView.setText(String.format("(%s)", Strings.join(synonymGroup.getMean(), ", ")));
+            holder.meanView.setVisibility(View.VISIBLE);
+            holder.meanView.setText(String.format("(%s)", Strings.join(synonymGroup.getMean(), ", ")));
         }
         return view;
     }
 
     public void setNewLookup(WordLookup lookup) {
         this.lookup = lookup;
+    }
+
+    static class LookupViewHolder {
+        @BindView(R.id.lookup_text)
+        TextView synonymView;
+        @BindView(R.id.lookup_mean)
+        TextView meanView;
+
+        LookupViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
     }
 }
